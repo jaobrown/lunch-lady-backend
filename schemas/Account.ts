e9ff57f3a0fef501ae98718f164083dcd560434f
Jared Brown <jared.brown@elementthree.com>
2021-06-06 23:48:59 -0400

    Add virtual fields to account

--- schemas/Account.ts
+++ schemas/Account.ts
@@ -21,7 +21,7 @@ export const Account = list({
           where: { account: { id: item.id } },
           resolveFields: 'id,amount',
         });
-        const totalBalance = allTransactions.reduce(function (
+        const totalBalance: number = allTransactions.reduce(function (
           tally: number,
           transaction: TransactionCreateInput
         ) {
@@ -29,6 +29,58 @@ export const Account = list({
         },
         0);
         return totalBalance;
+      },
+    }),
+    dateLastTextSent: virtual({
+      resolver: async (item, _, context) => {
+        const accountMessages = await context.lists.Message.findMany({
+          where: { account: { id: item.id } },
+          sortBy: ['timestamp_DESC'],
+          resolveFields: 'id,timestamp',
+        });
+        const dateLastTexted: 'string' = accountMessages[0]?.timestamp
+          ? accountMessages[0]?.timestamp
+          : '—';
+        return dateLastTexted;
+      },
+    }),
+    needsTexted: virtual({
+      graphQLReturnType: 'Boolean',
+      resolver: async (item, _, context) => {
+        const accountMessages = await context.lists.Message.findMany({
+          where: { account: { id: item.id } },
+          sortBy: ['timestamp_DESC'],
+          resolveFields: 'id,timestamp',
+        });
+        let needsTexted = false;
+        const lastTexted = new Date(accountMessages[0]?.timestamp).getDate();
+        const fiveDaysAgo = new Date().getDate() - 5;
+        if (lastTexted < fiveDaysAgo && item.phone !== null) {
+          needsTexted = true;
+        }
+        return needsTexted;
+      },
+    }),
+    timeline: virtual({
+      extendGraphQLTypes: [
+        'type TimelineEntry { timestamp: String, id: ID, amount: Int, textMessage: String, typename: String }',
+      ],
+      graphQLReturnType: '[TimelineEntry]',
+      resolver: async (item, _, context) => {
+        const accountMessages = await context.lists.Message.findMany({
+          where: { account: { id: item.id } },
+          resolveFields: 'id,timestamp,textMessage,__typename',
+        });
+        const accountTransactions = await context.lists.Transaction.findMany({
+          where: { account: { id: item.id } },
+          resolveFields: 'id,timestamp,amount,__typename',
+        });
+        const timeline = accountMessages
+          .concat(accountTransactions)
+          .sort((a, b) =>
+            a.timestamp < b.timestamp ? -1 : a.timestamp > b.timestamp ? 1 : 0
+          );
+        return timeline;
       },
     }),
   },
